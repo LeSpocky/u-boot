791132dd269392b1747b2e6689d1b29add4a6932
Alexander Dahl <ada@thorsis.com>
2019-01-30 16:16:03 +0100

    board: ncl: Remove the config.mk file
    For: v2013.10
    See: 77461a653803 ("arm: atmel: remove the config.mk file")

--- include/configs/at91sam9g20ncl.h
+++ include/configs/at91sam9g20ncl.h
@@ -23,6 +23,16 @@
 
 #include <asm/sizes.h>
 
+/*
+ * Warning: changing CONFIG_SYS_TEXT_BASE requires
+ * adapting the initial boot program (at91bootstrap).
+ * Since the linker has to swallow that define, we must use a pure
+ * hex number here!
+ */
+#ifndef CONFIG_SYS_TEXT_BASE
+#define CONFIG_SYS_TEXT_BASE		0x21e00000
+#endif
+
 /* ARM asynchronous clock */
 #define CONFIG_SYS_AT91_SLOW_CLOCK	32768		/* slow clock xtal */
 #define CONFIG_SYS_AT91_MAIN_CLOCK	18432000	/* main clock xtal */
