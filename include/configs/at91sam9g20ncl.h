92e3378630f072e05a390f648e0d19cfc8baa2ba
Alexander Dahl <ada@thorsis.com>
2019-01-30 16:08:43 +0100

    board: ncl: Add GPL-2.0+ SPDX-License-Identifier to source files
    For: v2013.10
    See: 1a4596601fd3 ("Add GPL-2.0+ SPDX-License-Identifier to source files")

--- include/configs/at91sam9g20ncl.h
+++ include/configs/at91sam9g20ncl.h
@@ -4,27 +4,12 @@
  * Lead Tech Design <www.leadtechdesign.com>
  *
  * Copyright 2010-2012 ifak system GmbH
+ * Copyright 2019 Thorsis Technologies GmbH
  *
  * Used configuation settings for the AT91SAM9260EK & AT91SAM9G20EK
  * boards for this AT91SAM9G20NCL configuration.
  *
- * See file CREDITS for list of people who contributed to this
- * project.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- * MA 02111-1307 USA
+ * SPDX-License-Identifier:	GPL-2.0+
  */
 
 #ifndef __CONFIG_H
