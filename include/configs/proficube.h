8e002c866eb0bae0070dffbb07a955d89159fcb7
Alexander Dahl <ada@thorsis.com>
2019-04-02 17:44:41 +0200

    configs: proficube: Define memory offsets and fdt file for automatic boot

--- include/configs/proficube.h
+++ include/configs/proficube.h
@@ -54,4 +54,16 @@
 
 #endif
 
+/* Environment */
+#define MEM_LAYOUT_ENV_SETTINGS \
+	"kernel_addr_r=0x22000000\0" \
+	"fdt_addr_r=0x23000000\0" \
+	"scriptaddr=0x23100000\0" \
+	"pxefile_addr_r=0x23200000\0" \
+	"ramdisk_addr_r=0x23300000\0"
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	MEM_LAYOUT_ENV_SETTINGS \
+	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0"
+
 #endif
