582c17fdc40345122ab7735e441e5e01ad5cbce6
Alexander Dahl <ada@thorsis.com>
2022-06-24 15:32:31 +0200

    board: thorsis: Follow up on Kconfig migrations
    Tested on ProfiCube only.
    
    Fixes: 49c8ef0e45a9 ("Convert CONFIG_SYS_LOAD_ADDR to Kconfig")
    Fixes: a2ac2b964bfb ("Convert CONFIG_SKIP_LOWLEVEL_INIT et al to Kconfig")
    Fixes: 7cfbba36e9f8 ("Convert CONFIG_SYS_MALLOC_LEN to Kconfig")
    Fixes: c0ad62c5f753 ("Convert CONFIG_SYS_NAND_ONFI_DETECTION to Kconfig")
    For: v2022.01

--- include/configs/proficube.h
+++ include/configs/proficube.h
@@ -12,18 +12,11 @@
 #define CONFIG_SYS_AT91_SLOW_CLOCK      32768
 #define CONFIG_SYS_AT91_MAIN_CLOCK      24000000 /* from 24 MHz crystal */
 
-#define CONFIG_SKIP_LOWLEVEL_INIT
-
-/* Size of malloc() pool */
-#define CONFIG_SYS_MALLOC_LEN		SZ_4M
-
 /* SDRAM */
 #define CONFIG_SYS_SDRAM_BASE		0x20000000
 
 #define CONFIG_SYS_INIT_SP_ADDR \
 	(CONFIG_SYS_SDRAM_BASE + SZ_16K - GENERATED_GBL_DATA_SIZE)
-
-#define CONFIG_SYS_LOAD_ADDR		0x22000000
 
 /* NAND flash */
 #ifdef CONFIG_NAND_BOOT
@@ -37,7 +30,6 @@
  * driver and the pio4 controller on SAMA5D2 …
  */
 #undef CONFIG_SYS_NAND_READY_PIN
-#define CONFIG_SYS_NAND_ONFI_DETECTION
 
 #endif
 
