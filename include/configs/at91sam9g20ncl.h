a58a47db147abfbe5044d19430625436ae16bbb9
Alexander Dahl <ada@thorsis.com>
2022-09-13 13:19:01 +0200

    configs: ncl: Migrate some CONFIG_ENV_ symbols to Kconfig
    For: v2019.10
    See: a9221f3ebd6d ("at91, omap2plus: configs: migrate CONFIG_ENV_ to defconfigs")
    See: 27a5d628275f ("at91: configs: Drop duplication of defconfig macros")

--- include/configs/at91sam9g20ncl.h
+++ include/configs/at91sam9g20ncl.h
@@ -103,9 +103,7 @@
 #if defined(CONFIG_NAND_BOOT)
 
 /* bootstrap + u-boot + env + linux in nandflash */
-#define CONFIG_ENV_OFFSET		0xA0000
 #define CONFIG_ENV_OFFSET_REDUND	0xE0000
-#define CONFIG_ENV_SIZE			0x40000		/* 1 sector = 128 kB  = 0x20000 */
 #define CONFIG_BOOTCOMMAND		"nand read 0x21800000 0x0120000 0x200000; bootm 0x21800000"
 
 #endif
