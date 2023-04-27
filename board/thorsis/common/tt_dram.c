23dabc5e5ea6bee32b3b957508a57202ea08bf96
Alexander Dahl <ada@thorsis.com>
2023-04-27 15:03:06 +0200

    board: thorsis: common: Add stub for SAMA5 SiP DRAM init
    We implemented a solution for automatic RAM size detection on ProfiCube
    and we also need that for Vera2 now.

---
+++ board/thorsis/common/tt_dram.c
@@ -0,0 +1,10 @@
+/*
+ * © 2023 Thorsis Technologies GmbH
+ */
+
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+int tt_dram_init_sama5_sip(void)
+{
+	return -1;
+}
---
+++ board/thorsis/common/tt_dram.h
@@ -0,0 +1,10 @@
+/*
+ * © 2023 Thorsis Technologies GmbH
+ */
+
+#ifndef TT_DRAM_H
+#define TT_DRAM_H
+
+int tt_dram_init_sama5_sip(void);
+
+#endif /* TT_DRAM_H */
