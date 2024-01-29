7c84c9d9e57b61576cda09087ed4620f3cf72c32
Alexander Dahl <ada@thorsis.com>
2024-01-29 10:30:56 +0100

    board/thorsis/vera2: Remove old NAND code
    We now rely on new sama5d2 NAND support only.
    (Make it less complicated for potential upstreaming.)

--- board/thorsis/vera2/vera2.c
+++ board/thorsis/vera2/vera2.c
@@ -19,40 +19,7 @@
 
 #include "../common/tt_dram.h"
 
-#ifdef CONFIG_NAND_ATMEL
-#include "../common/tt_nand.h"
-#endif
-
 DECLARE_GLOBAL_DATA_PTR;
-
-#ifdef CONFIG_NAND_ATMEL
-static void board_nand_hw_init(void)
-{
-	struct at91_smc *smc = (struct at91_smc *)ATMEL_BASE_SMC;
-
-	pr_debug("%s: called\n", __func__);
-
-	at91_periph_clk_enable(ATMEL_ID_HSMC);
-
-	/*
-	 * > 36.7.1 I/O Lines
-	 * > The pins used for interfacing the Static Memory Controller are
-	 * > multiplexed with the PIO lines. The programmer must first
-	 * > program the PIO controller to assign the Static Memory
-	 * > Controller pins to their peripheral function. If I/O lines of
-	 * > the SMC are not used by the application, they can be used for
-	 * > other purposes by the PIO controller.
-	 */
-
-	/* Configure data, NRD, and NWE lines (also needed for NAND!) */
-	tt_nand_configure_pio_datarw();
-
-	tt_nand_configure_pio_misc();
-
-	/* Configure SMC CS3 for NAND */
-	tt_nand_configure_smc(smc);
-}
-#endif
 
 #ifdef CONFIG_DEBUG_UART_BOARD_INIT
 static void board_uart1_hw_init(void)
@@ -82,10 +49,6 @@ int board_init(void)
 
 	/* address of boot parameters */
 	gd->bd->bi_boot_params = CFG_SYS_SDRAM_BASE + 0x100;
-
-#ifdef CONFIG_NAND_ATMEL
-	board_nand_hw_init();
-#endif
 
 	return 0;
 }
