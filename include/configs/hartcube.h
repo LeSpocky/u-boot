06657f51d5f4e9c7357dce579cbb5993920dff53
Alexander Dahl <ada@thorsis.com>
2024-03-20 15:27:51 +0100

    configs: hartcube: Enable (deprecated) distro_boot
    Essentially copied from MUXgate.  This is not the way we want to have
    the final boot support, but we need to get the board booting at all for
    now.

--- include/configs/hartcube.h
+++ include/configs/hartcube.h
@@ -16,4 +16,21 @@
 #define CFG_SYS_SDRAM_BASE		0x20000000
 #define CFG_SYS_SDRAM_SIZE		SZ_64M
 
+/* Environment */
+#define MEM_LAYOUT_ENV_SETTINGS \
+	"kernel_addr_r=0x22000000\0" \
+	"fdt_addr_r=0x23000000\0" \
+	"scriptaddr=0x23100000\0" \
+	"pxefile_addr_r=0x23200000\0" \
+	"ramdisk_addr_r=0x23300000\0"
+
+#define BOOT_TARGET_DEVICES(func) \
+	func(UBIFS, ubifs, 0, UBI, boot)
+#include <config_distro_bootcmd.h>
+
+#define CFG_EXTRA_ENV_SETTINGS \
+	MEM_LAYOUT_ENV_SETTINGS \
+	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0" \
+	BOOTENV
+
 #endif
