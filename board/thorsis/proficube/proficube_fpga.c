95daa8c6ad5173b6e7db10b12e2b7f0974b709c6
Alexander Dahl <ada@thorsis.com>
2024-10-01 11:18:42 +0200

    board: thorsis: proficube: Remove alternative bitrev8() implementation
    CONFIG_BITREVERSE is available since U-Boot v2013.07, why was that added
    in the first place?

--- board/thorsis/proficube/proficube_fpga.c
+++ board/thorsis/proficube/proficube_fpga.c
@@ -40,22 +40,7 @@ static Altera_desc *proficube_get_fpga_desc(void);
 #ifdef CONFIG_BITREVERSE
 #include <linux/bitrev.h>
 #else
-#include <limits.h>
-static uint8_t bitrev8(uint8_t v)
-{
-	unsigned int r = v; /* r will be reversed bits of v; first get LSB of v	*/
-	int s = sizeof(v) * CHAR_BIT - 1; /* extra shift needed at end */
-
-	for (v >>= 1; v; v >>= 1) {
-		r <<= 1;
-		r |= v & 1;
-		s--;
-	}
-
-	r <<= s; /* shift when v's highest bits are zero	*/
-
-	return r;
-}
+#error "enable CONFIG_BITREVERSE"
 #endif
 #endif
 
